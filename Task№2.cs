b0250b0c76d46b76d8578038a08b092a8f51fb66
Leakyyyyy <145537370+Leakyyyyy@users.noreply.github.com>
2024-02-09 16:32:03 +0700

    Create Task№2.cs

---
+++ Task№2.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Вводим значение числа x с клавиатуры
+        Console.Write("Введите число x (x >= 100): ");
+        int x = int.Parse(Console.ReadLine());
+
+        // Получаем вторую цифру числа x
+        int secondDigit = (x / 10) % 10;
+
+        // Зачеркиваем вторую цифру числа x
+        int newX = x / 100 * 10 + x % 10;
+
+        // Получаем число n, добавляя зачеркнутую вторую цифру x в конец числа newX
+        int n = newX * 10 + secondDigit;
+
+        // Выводим полученное значение числа n
+        Console.WriteLine("n = " + n);
+    }
+}
